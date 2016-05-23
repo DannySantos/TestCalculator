e7d34a227ebc2112b74b61abeb6280fa352cc112
Danny Santos <danny@ethicalswitch.com>
2016-05-23 11:58:41 +0100

    Add, subtract, multiply, divide implementation

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,19 @@
 class Calculator
   attr_accessor :accumulator
+  
+  def add(num)
+    @accumulator + num
+  end
+  
+  def subtract(num)
+    @accumulator - num
+  end
+  
+  def multiply(num)
+    @accumulator * num
+  end
+  
+  def divide(num)
+    @accumulator / num
+  end
 end
