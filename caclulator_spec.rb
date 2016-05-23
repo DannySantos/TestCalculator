47bc7467c6401e2d7890fad9879540f552dfc2c1
Danny Santos <danny@ethicalswitch.com>
2016-05-23 11:55:33 +0100

    Add, subtract, multiply, divide tests written

--- caclulator_spec.rb
+++ caclulator_spec.rb
@@ -4,8 +4,32 @@ require './calculator'
 class CalculatorSpec < MiniTest::Test
   def test_accumulator
     my_calculator = Calculator.new
-    my_calculator.accumulator = 5
-    assert_equal(my_calculator.accumulator, 5)
+    my_calculator.accumulator = 12
+    assert_equal(my_calculator.accumulator, 12)
+  end
+  
+  def test_add
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 12
+    assert_equal(my_calculator.add(2), 14)
+  end
+  
+  def test_subtract
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 12
+    assert_equal(my_calculator.subtract(2), 10)
+  end
+  
+  def test_multiply
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 12
+    assert_equal(my_calculator.multiply(2), 24)
+  end
+  
+  def test_divide
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 12
+    assert_equal(my_calculator.divide(2), 6)
   end
 end
 
