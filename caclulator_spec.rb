fcb5bbc4ec04ef9e6a46fe519f0779337f6f4cf8
Danny Santos <danny@ethicalswitch.com>
2016-05-23 12:14:11 +0100

    test accumulator to be floating point

--- caclulator_spec.rb
+++ caclulator_spec.rb
@@ -5,31 +5,37 @@ class CalculatorSpec < MiniTest::Test
   def test_accumulator
     my_calculator = Calculator.new
     my_calculator.accumulator = 12
-    assert_equal(my_calculator.accumulator, 12)
+    assert_equal(12, my_calculator.accumulator)
+  end
+  
+  def test_floating_point
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 12
+    assert_instance_of(Float, my_calculator_.accumulator)
   end
   
   def test_add
     my_calculator = Calculator.new
     my_calculator.accumulator = 12
-    assert_equal(my_calculator.add(2), 14)
+    assert_equal(14, my_calculator.add(2))
   end
   
   def test_subtract
     my_calculator = Calculator.new
     my_calculator.accumulator = 12
-    assert_equal(my_calculator.subtract(2), 10)
+    assert_equal(10, my_calculator.subtract(2))
   end
   
   def test_multiply
     my_calculator = Calculator.new
     my_calculator.accumulator = 12
-    assert_equal(my_calculator.multiply(2), 24)
+    assert_equal(24, my_calculator.multiply(2))
   end
   
   def test_divide
     my_calculator = Calculator.new
     my_calculator.accumulator = 12
-    assert_equal(my_calculator.divide(2), 6)
+    assert_equal(6, my_calculator.divide(2))
   end
 end
 
--- calculator.rb
+++ calculator.rb
@@ -2,18 +2,18 @@ class Calculator
   attr_accessor :accumulator
   
   def add(num)
-    @accumulator + num
+    @accumulator += num
   end
   
   def subtract(num)
-    @accumulator - num
+    @accumulator -= num
   end
   
   def multiply(num)
-    @accumulator * num
+    @accumulator *= num
   end
   
   def divide(num)
-    @accumulator / num
+    @accumulator /= num
   end
 end
